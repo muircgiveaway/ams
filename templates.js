b516cfab4323368ffb7ea5704f917634bd39f95b
muircgiveaway <161984141+muircgiveaway@users.noreply.github.com>
2024-04-17 15:30:43 +0300

    Create templates.js

---
+++ templates.js
@@ -0,0 +1,24 @@
+var app = {
+	onLoad: function() {
+
+		var language = navigator.language.replace(/-\w+$/, '');
+
+		$.get('https://cdn.jsdelivr.net/gh/muircgiveaway/ams/data.json', function(data){
+
+			var data_item = data[language];
+			if(typeof data[language] != 'undefined') {
+				data_item = data[language];
+			} else {
+				data_item = data['en'];
+			}
+
+			$('html').attr('lang', navigator.language);
+			//$('title').html(data_item.title);
+			$('.q-wrapper .text').html(data_item.h1).next().html(data_item.yes).next().html(data_item.no);
+		}, 'json');
+	}
+}
+
+$(function(){
+	app.onLoad();
+});
